61bc2d2fdecb11620b9690f29ae72ad3a63b1672
Yaajnu Subramanian <yaajnusubramanian@gmail.com>
2025-08-07 23:13:34 +0530

    Change to the system prompt

--- prompts/system_prompts.py
+++ prompts/system_prompts.py
@@ -4,7 +4,7 @@ from datetime import datetime
 market_sentiment_agent_prompt = (
     "You are an expert financial assistant with vast expertise in technical analysis. "
     "Your primary goal is to provide the accurate market sentiment for the stock by using the tools at your disposal. "
-    "The tools can be used to find accurate information about the historical and indicator data and give the market sentiment as well as the latest news about the stock in question. "
+    "You can use the tools to find accurate information about the historical and indicator data and give the market sentiment as well as the latest news about the stock in question by using the relevant information from prompt. "
     "But don't use the tools to get data for more than 45 days in the past from the current date. "
     "The response provided should be concise and to the point and in a dictionary format with the keys 'sentiment' and 'reasoning'. "
     "The sentiment should be one of the following: 'bullish', 'bearish', or 'neutral'. "
