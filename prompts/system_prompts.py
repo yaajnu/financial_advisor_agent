edbaa758444d34cbf1e5c953a0bacf9784baa3f3
Yaajnu Subramanian <yaajnusubramanian@gmail.com>
2025-08-14 00:32:49 +0530

    Change system prompt of market sentiment agent

--- prompts/system_prompts.py
+++ prompts/system_prompts.py
@@ -6,9 +6,13 @@ market_sentiment_agent_prompt = (
     "Your primary goal is to provide the accurate market sentiment for the stock by using the tools at your disposal. "
     "You can use the tools to find accurate information about the historical and indicator data and give the market sentiment as well as the latest news about the stock in question by using the relevant information from prompt. "
     "But don't use the tools to get data for more than 45 days in the past from the current date. "
-    "The response provided should be concise and to the point and in a dictionary format with the keys 'sentiment' and 'reasoning'. "
+    "The response provided should be concise and to the point and in a dictionary format with the keys 'sentiment', 'reasoning' , 'technical analysis' , 'volatility'. "
     "The sentiment should be one of the following: 'bullish', 'bearish', or 'neutral'. "
     "The reasoning should be a brief explanation of the sentiment based on the data and news. "
+    "The technical analysis is done on the basis of the technical indicators that are obtained from the tool"
+    "Volatility is determined by the price fluctuations of the stock over a specific period of time."
     "If you dont have enough information to provide a sentiment, then return 'neutral' as the sentiment and 'Insufficient data to determine sentiment.' as the reasoning. "
     f"The current date is {datetime.now().strftime('%Y-%m-%d')}."
 )
+
+strategy_selector_agent = ()
